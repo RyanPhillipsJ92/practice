3ce1608c718c643b47c78d128040c0dde475fba1
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-04-07 13:54:29 -0500

    working on manipulating strings and showing output

---
+++ src/ReverseWordsInString.java
@@ -0,0 +1,22 @@
+public class ReverseWordsInString {
+
+    public static String reverse(String input) {
+        if (input.isEmpty()) {
+            return input;
+        }
+        String[] arr = input.split(" ", 2);
+        String firstWord = arr[0];
+        String remainingSentence;
+        if (arr.length == 2)
+            remainingSentence = arr[1];
+        else
+            remainingSentence = "";
+        return reverse(remainingSentence) + firstWord + " ";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("this is a TEST"));
+        System.out.println(reverse("backwards! typed is sentence This"));
+
+    }
+}
